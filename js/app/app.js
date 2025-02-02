ca23089e87a5cfb6c875b685b4359c19a79316f4
ivanisovmaks <166316568+ivanisovmaks@users.noreply.github.com>
2025-02-02 21:39:37 +0200

    Add files via upload

--- js/app/app.js
+++ js/app/app.js
@@ -1,8 +1,8 @@
 import {router} from './router.js';
 import {msg} from './widgets/msg.js';
-// import {header} from './widgets/header.js';
+import {header} from './widgets/header.js';
 // import {search} from './widgets/search.js';
-// import {popup} from './widgets/popup.js';
+import {popup} from './widgets/popup.js';
 
 // import {toogle} from './widgets/toogle.js';
 // import {img} from './widgets/img.js';
@@ -65,19 +65,19 @@ document.addEventListener('DOMContentLoaded', function(){
             var fd = new FormData();
 
             for (var x in obj) {
-              if (typeof obj[x] === 'object' && x !== 'img' && x !== 'copy') {
-                for (var y in obj[x]) {
-                  if (typeof obj[x][y] === 'object') {
-                    for (var z in obj[x][y]) {
-                      fd.append(x+'['+y+']['+z+']', obj[x][y][z]);
+                if (typeof obj[x] === 'object' && x !== 'img' && x !== 'copy') {
+                    for (var y in obj[x]) {
+                        if (typeof obj[x]==='object') {
+                            for (var z in obj[x][y]) {
+                                fd.append(x+'['+y+']['+z+']', obj[x][y][z]);
+                            }
+                        } else {
+                            fd.append(x+'['+y+']', obj[x][y]);
+                        }
                     }
-                  } else {
-                    fd.append(x+'['+y+']', obj[x][y]);
-                  }
+                }else if (x != 'copy') {
+                    fd.append(x, obj[x]);
                 }
-              } else if (x != 'copy') {
-                fd.append(x, obj[x]);
-              }
             }
 
             return fd;
@@ -86,6 +86,8 @@ document.addEventListener('DOMContentLoaded', function(){
       };
     
     var app = Vue.createApp(main)
+    .component('Header',header)
+    .component('popup',popup)
     .component('msg',msg)
     .use(router)
     .mount('#content')
